041edc77040ba293e2aa0a75f731ad8b8aa740ab
Phil Aquilina <philaquilina@gmail.com>
2016-03-31 10:36:39 -0700

    refactor gestureLevenshtein

--- src/gestureLevenshtein.js
+++ src/gestureLevenshtein.js
@@ -8,18 +8,8 @@ const gestureLevenshtein = (a, b) => {
   if (a.length === 0 || b.length === 0) {
     return BIG_NUM;
   }
-  // setup a distance matrix of all zeroes
-  const distMatrix = times(b.length + 1, () => {
-    return times(a.length + 1, () => 0);
-  });
-  // fill the distance matrix in
-  for (let i=1; i<=b.length; i++) {
-    for (let j=1; j<=a.length; j++) {
-      distMatrix[i][j] = sectorDistance(a[j-1], b[i-1]);
-    }
-  }
 
-  // next create a levenshtein matrix
+  // create a levenshtein matrix
   const levMatrix = times(b.length + 1, () => {
     return times(a.length + 1, () => 0);
   });
@@ -34,7 +24,7 @@ const gestureLevenshtein = (a, b) => {
   // now compute the cells in the levenshtein matrix
   for (let i=1; i<=b.length; i++) {
     for (let j=1; j<=a.length; j++) {
-      const cost = distMatrix[i][j];
+      const cost = sectorDistance(a[j-1], b[i-1]);
       levMatrix[i][j] = Math.min(
         cost + levMatrix[i-1][j],
         cost + levMatrix[i][j-1],

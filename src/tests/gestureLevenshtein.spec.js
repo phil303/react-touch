02ee5c7bce31d4875cfe11bd5a58135394b7faab
Phil Aquilina <philaquilina@gmail.com>
2016-03-31 10:42:41 -0700

    terser syntax for tests

--- src/tests/gestureLevenshtein.spec.js
+++ src/tests/gestureLevenshtein.spec.js
@@ -20,30 +20,30 @@ const CIRCLE = [
 
 describe("gestureLevenshtein", () => {
   it("should return a big number when an argument is an empty string", () => {
-    expect(gestureLevenshtein("", "1")).to.be.equal(10000);
-    expect(gestureLevenshtein("1", "")).to.be.equal(10000);
+    expect(gestureLevenshtein("", "1")).to.equal(10000);
+    expect(gestureLevenshtein("1", "")).to.equal(10000);
   });
 
   it("should return 0 when the arguments match", () => {
-    expect(gestureLevenshtein(UPCARET, UPCARET)).to.be.equal(0);
-    expect(gestureLevenshtein(CIRCLE, CIRCLE)).to.be.equal(0);
+    expect(gestureLevenshtein(UPCARET, UPCARET)).to.equal(0);
+    expect(gestureLevenshtein(CIRCLE, CIRCLE)).to.equal(0);
   });
 
   it("should return 0 when the arguments 'collapse' to be equal", () => {
     // Collapse here meaning if we were to manually remove consecutive
     // duplicates, the below value would collapse into "71".
     const humanUpCaret = "777777771111111"
-    expect(gestureLevenshtein(UPCARET, humanUpCaret)).to.be.equal(0);
+    expect(gestureLevenshtein(UPCARET, humanUpCaret)).to.equal(0);
 
     const humanCircle = "0111122222233445555666700000";
-    expect(gestureLevenshtein(CIRCLE, humanCircle)).to.be.equal(0);
+    expect(gestureLevenshtein(CIRCLE, humanCircle)).to.equal(0);
   });
 
   it("should return correct values when arguments don't match", () => {
     const humanUpCaret = "77777777001111111"
-    expect(gestureLevenshtein(UPCARET, humanUpCaret)).to.be.equal(2);
+    expect(gestureLevenshtein(UPCARET, humanUpCaret)).to.equal(2);
 
     const humanCircle = "0001121112222122233344445555665666777770";
-    expect(gestureLevenshtein(CIRCLE, humanCircle)).to.be.equal(3);
+    expect(gestureLevenshtein(CIRCLE, humanCircle)).to.equal(3);
   });
 });

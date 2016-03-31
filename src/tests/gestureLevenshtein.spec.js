1f4aa3626344c18d68887ceaff1d4e6a017cbeed
Phil Aquilina <philaquilina@gmail.com>
2016-03-31 10:47:36 -0700

    eslint fixes

--- src/tests/gestureLevenshtein.spec.js
+++ src/tests/gestureLevenshtein.spec.js
@@ -14,7 +14,7 @@ const CIRCLE = [
   moves.UPLEFT,
   moves.UP,
   moves.UPRIGHT,
-  moves.RIGHT
+  moves.RIGHT,
 ];
 
 
@@ -32,7 +32,7 @@ describe("gestureLevenshtein", () => {
   it("should return 0 when the arguments 'collapse' to be equal", () => {
     // Collapse here meaning if we were to manually remove consecutive
     // duplicates, the below value would collapse into "71".
-    const humanUpCaret = "777777771111111"
+    const humanUpCaret = "777777771111111";
     expect(gestureLevenshtein(UPCARET, humanUpCaret)).to.equal(0);
 
     const humanCircle = "0111122222233445555666700000";
@@ -40,7 +40,7 @@ describe("gestureLevenshtein", () => {
   });
 
   it("should return correct values when arguments don't match", () => {
-    const humanUpCaret = "77777777001111111"
+    const humanUpCaret = "77777777001111111";
     expect(gestureLevenshtein(UPCARET, humanUpCaret)).to.equal(2);
 
     const humanCircle = "0001121112222122233344445555665666777770";
